5fe1515d9a7a6689595468d213e6158e2af0d211
carlosdelacruzj <delacruzcarlos1405@gmail.com>
2025-09-10 01:10:16 -0500

    fix(routes): convierte drive en router y monta /upload-file (compatibilidad)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,10 @@
 const { Router } = require("express");
 const router = Router();
 
-// Solo los que ya montabas en app.js
 router.use(require("./core"));
 router.use(require("./proyecto"));
 router.use(require("./mail"));
-// Más adelante, cuando confirmemos que exportan un router válido:
-// router.use(require("./mail"));
-// router.use(require("./drive"));
-// router.use(require("./validacionCorreo"));
+router.use(require("./drive"));
+router.use(require("./validacionCorreo"));
 
 module.exports = router;
